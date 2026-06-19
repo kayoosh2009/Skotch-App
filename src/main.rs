ab60885d37286fee83491dc4d3cc30a743d39fef
kayoosh2009 <kayoosh2009@gmail.com>
2026-06-19 16:14:11 +0300

    0.0.0000001

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> &'static str {
+    "Scotch API"
+}
